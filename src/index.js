65ed408b8d788cf0b24205a3bd11b167eae05dd9
longph <longphamduy2002@gmail.com>
2024-09-04 19:53:24 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5173;
+
+app.get("/", (req, res) => res.send("Express on Vercelll"));
+
+app.listen(PORT, () => console.log(`Server ready on port http://localhost:${PORT}.`));
+
+module.exports = app;
