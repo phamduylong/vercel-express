45a332ce1487fc00bba0b4fc096dbd2c951b9cd9
longph <longphamduy2002@gmail.com>
2024-09-13 19:56:31 +0300

    fix typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5173;
 
-app.get("/", (req, res) => res.send("Express on Vercelll"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(PORT, () => console.log(`Server ready on port http://localhost:${PORT}.`));
 
