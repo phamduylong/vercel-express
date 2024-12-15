bc1225a37676e450358603b7c92cedd26fad75f5
longph <longphamduy2002@gmail.com>
2024-12-15 20:04:09 +0200

    Stupid change to test deploy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5173;
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(PORT, () => console.log(`Server ready on port http://localhost:${PORT}.`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}.`));
 
 module.exports = app;
